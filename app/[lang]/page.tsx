de5b479962cd6e623d91b62a50d487076c702961
Matthes-Baer <Matthes.baer@yahoo.de>
2023-05-31 15:42:20 +0200

    feat: Added to-do notes

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,8 +1,6 @@
 import type { INormalPageProps } from "@/utils/interfaces";
 
 export default function Home(props: INormalPageProps) {
-  console.log(props.params.lang);
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Main</div>
@@ -11,6 +9,16 @@ export default function Home(props: INormalPageProps) {
         <li>
           Vor Build im types file ggf. @ts-ignore hinzufügen, wenn etwas nicht
           funktioniert; siehe layout
+        </li>
+      </ul>
+
+      <ul>
+        <li>To-do:</li>
+        <li>Weitere Extra Files anschauen (loading.tsx)</li>
+        <li>Eigene Google Font implementieren</li>
+        <li>
+          Tailwind Config anfänglich bearbeiten mit Farbpalette - muss
+          vermutlich öfters überarbeitet werden anfangs
         </li>
       </ul>
     </main>
