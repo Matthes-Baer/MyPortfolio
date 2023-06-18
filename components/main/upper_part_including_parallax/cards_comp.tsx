8acb3f9f419ad06b12ad2d8f1465715a9538296d
Matthes-Baer <Matthes.baer@yahoo.de>
2023-06-18 05:57:41 +0200

    feat: Worked on cards content styling/animation/structure/fetching

--- components/main/upper_part_including_parallax/cards_comp.tsx
+++ components/main/upper_part_including_parallax/cards_comp.tsx
@@ -17,8 +17,11 @@ import ALL_OPENED_CARDS_COMP from "./all_opened_cards_comp";
 import { gsap } from "gsap";
 import CARD_CONTENT_COMP from "./card_content_comp";
 import Loading from "@/app/[lang]/loading";
+import { useParams } from "next/navigation";
 
 const CARDS_COMP: () => JSX.Element = (): JSX.Element => {
+  const lang: string = useParams().lang;
+
   const [current_card_idx_count, set_current_card_idx_count]: [
     number,
     Dispatch<SetStateAction<number>>
@@ -31,9 +34,15 @@ const CARDS_COMP: () => JSX.Element = (): JSX.Element => {
     boolean,
     Dispatch<SetStateAction<boolean>>
   ] = useState<boolean>(false);
+  const [first_fetch, set_first_fetch]: [
+    boolean,
+    Dispatch<SetStateAction<boolean>>
+  ] = useState<boolean>(false);
 
-  const container_ref: MutableRefObject<null> = useRef<null>(null);
+  const front_card_text_ref: MutableRefObject<null> = useRef<null>(null);
+  const front_card_image_ref: MutableRefObject<null> = useRef<null>(null);
   const moving_card_ref: MutableRefObject<null> = useRef<null>(null);
+  const start_info_text_ref: MutableRefObject<null> = useRef<null>(null);
 
   const fetch_stuff = useCallback(async () => {
     let fetch_button_disable_timeout: NodeJS.Timeout;
@@ -41,7 +50,24 @@ const CARDS_COMP: () => JSX.Element = (): JSX.Element => {
 
     const animate_card = () => {
       const moving_card = moving_card_ref.current;
+      const front_card_image = front_card_image_ref.current;
       const moving_card_timeline = gsap.timeline();
+
+      if (!first_fetch) {
+        gsap.to(front_card_image, {
+          opacity: 1,
+          duration: 0.5,
+          ease: "easeInOut",
+        });
+
+        set_first_fetch(true);
+      }
+
+      gsap.to(start_info_text_ref.current, {
+        opacity: 0,
+        duration: 0.1,
+        ease: "linear",
+      });
 
       moving_card_timeline.to(moving_card, {
         xPercent: 150,
@@ -68,7 +94,8 @@ const CARDS_COMP: () => JSX.Element = (): JSX.Element => {
 
     try {
       let data: ICard;
-      const container = container_ref.current;
+      const front_card_text = front_card_text_ref.current;
+
       animate_card();
 
       let res: Response = await fetch("/api/POST_card_content", {
@@ -87,7 +114,7 @@ const CARDS_COMP: () => JSX.Element = (): JSX.Element => {
       }, 2000);
 
       gsap.fromTo(
-        container,
+        front_card_text,
         { opacity: 0 },
         { opacity: 1, duration: 0.5, ease: "easeInOut" }
       );
@@ -99,7 +126,7 @@ const CARDS_COMP: () => JSX.Element = (): JSX.Element => {
     }
 
     return () => clearTimeout(fetch_button_disable_timeout);
-  }, [current_card_idx_count]);
+  }, [current_card_idx_count, first_fetch]);
 
   return (
     <Suspense fallback={<Loading />}>
@@ -107,13 +134,31 @@ const CARDS_COMP: () => JSX.Element = (): JSX.Element => {
         className="absolute w-full z-50"
         style={{ top: "50%", left: 0, transform: "translate(0, -50%)" }}
       >
+        {!first_fetch && (
+          <div
+            className="text-xl bg-dark_gray_stone p-5 rounded text-sm"
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              zIndex: 9000,
+            }}
+            ref={start_info_text_ref}
+          >
+            {lang === "en"
+              ? "Click on the left card to reveal skill cards."
+              : "Klicke auf die linke Karte, um Skill-Karten aufzudecken."}
+          </div>
+        )}
+
         <div className="flex justify-evenly items-center">
           <div className="relative">
             <button
               className="w-full h-full"
               onClick={fetch_stuff}
               disabled={
-                (current_card_idx_count > 3 ? true : false) ||
+                (current_card_idx_count > 2 ? true : false) ||
                 fetch_button_disabled
               }
             >
@@ -129,14 +174,22 @@ const CARDS_COMP: () => JSX.Element = (): JSX.Element => {
               style={{ zIndex: -1 }}
             />
           </div>
+
           <div className="relative">
-            <div ref={container_ref}>
+            <div ref={front_card_text_ref}>
               {opened_cards.length > 0 ? (
                 <CARD_CONTENT_COMP opened_card={opened_cards.at(-1)!} />
               ) : null}
             </div>
 
-            <Image src={card_front} height={500} width={500} alt="Test" />
+            <Image
+              src={card_front}
+              height={500}
+              width={500}
+              alt="Test"
+              style={{ opacity: 0 }}
+              ref={front_card_image_ref}
+            />
           </div>
         </div>
         {opened_cards.length > 0 ? (
