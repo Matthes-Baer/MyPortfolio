9f2ccb452700aacf61e74eb3fcbd0afe1f28cd8c
Matthes-Baer <Matthes.baer@yahoo.de>
2023-06-26 23:44:54 +0200

    feat: Added project images

--- utils/import_images.ts
+++ utils/import_images.ts
@@ -211,81 +211,185 @@ export const TECHSTACK_IMAGES: IImages = {
 
 //? For the bigger images
 export const PROJECT_IMAGES: IProjects = {
-  Divid: [
+  blenumentation: [
     {
-      src: require("public/main_images/project_images/task_tour/Loading.jpg"),
+      src: require("public/main_images/project_images/blenumentation/home_firstpic.jpg"),
+      alt: {
+        en: "Image representing one part of the homepage",
+        de: "Bild, das einen Teil der Homepage darstellt",
+      },
+    },
+    {
+      src: require("public/main_images/project_images/blenumentation/home_secondpic.jpg"),
+      alt: {
+        en: "Image representing one part of the homepage",
+        de: "Bild, das einen Teil der Homepage darstellt",
+      },
+    },
+    {
+      src: require("public/main_images/project_images/blenumentation/basics.jpg"),
+      alt: {
+        en: "Image representing the basics page route",
+        de: "Bild, das die Basics-Route darstellt",
+      },
+    },
+    {
+      src: require("public/main_images/project_images/blenumentation/materials.jpg"),
+      alt: {
+        en: "Image representing the materials page route",
+        de: "Bild, das die Materials-Route darstellt",
+      },
+    },
+    {
+      src: require("public/main_images/project_images/blenumentation/hotkeys.jpg"),
+      alt: {
+        en: "Image representing the hotkeys page route",
+        de: "Bild, das die Hotkeys-Route darstellt",
+      },
+    },
+  ],
+  task_tour: [
+    {
+      src: require("public/main_images/project_images/task_tour/darkmode_firstpic.jpg"),
+      alt: {
+        en: "Image representing the upper part of application",
+        de: "Bild, das den oberen Teil der Anwendung darstellt",
+      },
+    },
+    {
+      src: require("public/main_images/project_images/task_tour/darkmode_secondpic.jpg"),
+      alt: {
+        en: "Image representing the lower part of application",
+        de: "Bild, das den unteren Teil der Anwendung darstellt",
+      },
+    },
+    {
+      src: require("public/main_images/project_images/task_tour/darkmode_thirdpic.jpg"),
+      alt: {
+        en: "Image representing the lower part of application with to-do tasks",
+        de: "Bild, das den unteren Teil der Anwendung mit To-Do-Aufgaben darstellt",
+      },
+    },
+    {
+      src: require("public/main_images/project_images/task_tour/lightmode_firstpic.jpg"),
+      alt: {
+        en: "Image representing light mode",
+        de: "Bild, das den Lightmode darstellt",
+      },
+    },
+  ],
+  divid: [
+    {
+      src: require("public/main_images/project_images/divid/Loading.jpg"),
       alt: {
         en: "Image representing the initial loading state of the app",
         de: "Bild, das den anfänglichen Ladezustand der Anwendung darstellt",
       },
     },
     {
-      src: require("public/main_images/project_images/task_tour/Start.jpg"),
+      src: require("public/main_images/project_images/divid/Start.jpg"),
       alt: {
         en: "Image representing the start screen with a login and register option",
         de: "Bild, das den Startbildschirm mit einer Anmelde- und Registrierungsoption darstellt",
       },
     },
     {
-      src: require("public/main_images/project_images/task_tour/Home.jpg"),
+      src: require("public/main_images/project_images/divid/Home.jpg"),
       alt: {
         en: "Image representing the home screen with the user's profile",
         de: "Bild, das den Startbildschirm mit dem Profil des Nutzers darstellt",
       },
     },
     {
-      src: require("public/main_images/project_images/task_tour/Gamemode.jpg"),
+      src: require("public/main_images/project_images/divid/Gamemode.jpg"),
       alt: {
         en: "Image representing the game screen where you choose the different game modes",
         de: "Bild des Spielbildschirms, auf dem die verschiedenen Spielmodi ausgewählt werden",
       },
     },
     {
-      src: require("public/main_images/project_images/task_tour/Instructions.jpg"),
+      src: require("public/main_images/project_images/divid/Instructions.jpg"),
       alt: {
         en: "Image representing the instructions for the game",
         de: "Bild, das die Spielanleitung darstellt",
       },
     },
     {
-      src: require("public/main_images/project_images/task_tour/Game.jpg"),
+      src: require("public/main_images/project_images/divid/Game.jpg"),
       alt: {
         en: "Image representing the game screen with an active game session",
         de: "Bild des Spielbildschirms mit einer aktiven Spielsitzung",
       },
     },
     {
-      src: require("public/main_images/project_images/task_tour/Scores.jpg"),
+      src: require("public/main_images/project_images/divid/Scores.jpg"),
       alt: {
         en: "Image representing the scores screen",
         de: "Bild, das den Score-Bildschirm darstellt",
       },
     },
     {
-      src: require("public/main_images/project_images/task_tour/Store.jpg"),
+      src: require("public/main_images/project_images/divid/Store.jpg"),
       alt: {
         en: "Image representing the store screen",
         de: "Bild, das den Store-Bildschirm darstellt",
       },
     },
   ],
+  nutrition_calculator: [
+    {
+      src: require("public/main_images/project_images/nutrition_calculator/home_firstpic.jpg"),
+      alt: {
+        en: "Image representing the upper part of the homepage",
+        de: "Bild, das den oberen Teil der Homepage darstellt",
+      },
+    },
+    {
+      src: require("public/main_images/project_images/nutrition_calculator/home_secondpic.jpg"),
+      alt: {
+        en: "Image representing the middle part of the homepage",
+        de: "Bild, das den mittleren Teil der Homepage darstellt",
+      },
+    },
+    {
+      src: require("public/main_images/project_images/nutrition_calculator/home_thirdpic.jpg"),
+      alt: {
+        en: "Image representing the lower part of the homepage",
+        de: "Bild, das den unteren Teil der Homepage darstellt",
+      },
+    },
+    {
+      src: require("public/main_images/project_images/nutrition_calculator/details.jpg"),
+      alt: {
+        en: "Image representing the dynamic details page route",
+        de: "Bild, das die dynamisch generierte Details-Route darstellt",
+      },
+    },
+    {
+      src: require("public/main_images/project_images/nutrition_calculator/about_page.jpg"),
+      alt: {
+        en: "Image representing the about page route",
+        de: "Bild, das die About-Route darstellt",
+      },
+    },
+  ],
   no_framework_project: [
     {
-      src: require("public/main_images/project_images/no_framework_project/no_framework_project_first_pic.png"),
+      src: require("public/main_images/project_images/no_framework_project/no_framework_project_first_pic.jpg"),
       alt: {
         en: "Image representing one part of the webpage",
         de: "Bild, das einen Teil der Webseite darstellt",
       },
     },
     {
-      src: require("public/main_images/project_images/no_framework_project/no_framework_project_second_pic.png"),
+      src: require("public/main_images/project_images/no_framework_project/no_framework_project_second_pic.jpg"),
       alt: {
         en: "Image representing one part of the webpage",
         de: "Bild, das einen Teil der Webseite darstellt",
       },
     },
     {
-      src: require("public/main_images/project_images/no_framework_project/no_framework_project_third_pic.png"),
+      src: require("public/main_images/project_images/no_framework_project/no_framework_project_third_pic.jpg"),
       alt: {
         en: "Image representing one part of the webpage",
         de: "Bild, das einen Teil der Webseite darstellt",
