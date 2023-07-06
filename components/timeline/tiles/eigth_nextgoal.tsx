09a4fcab044367ee3905123a49f871eeab8007dd
Matthes-Baer <Matthes.baer@yahoo.de>
2023-07-06 23:15:46 +0200

    feat: Updated timeline background styling with new images and focused on making it work for all screen sizes, including manuaally tracking the screen sizes with an eventlistener

--- components/timeline/tiles/eigth_nextgoal.tsx
+++ components/timeline/tiles/eigth_nextgoal.tsx
@@ -3,12 +3,12 @@ import TEMPLATE_INNER_PART from "./template_inner_part";
 const EIGHT_NEXTGOAL: (props: { language: string }) => JSX.Element = (props: {
   language: string;
 }): JSX.Element => {
-  const title = props.language === "de" ? "Nächstes Ziel" : "Next Goal";
+  const title = props.language === "de" ? "Nächstes Ziel" : "Next Objective";
   const content =
     props.language === "de"
-      ? "Ziel Front-End-Entwickler"
-      : "Goal front end developer";
-  const date = props.language === "de" ? "x" : "x";
+      ? "Mein nächstes übergeordnetes Ziel besteht darin, ein professioneller Front-End-Entwickler zu werden, um weitere Webentwicklungsprojekte umzusetzen und mehr praktische Erfahrungen zu sammeln. Mittelfristig habe ich außerdem die Ambition, mich zu einem professionellen Fullstack-Entwickler weiterzuentwickeln, um einen ganzheitlichen Mehrwert für alle Projekte zu schaffen, an denen ich beteiligt bin."
+      : "My next overall goal is to become a professional front-end developer to implement more web development projects and gain more hands-on experience. In the medium term, I also have the ambition to develop into a professional full-stack developer to add holistic value to all projects I am involved in.";
+  const date = props.language === "de" ? "" : "";
 
   return (
     <div>
--- components/timeline/timeline_part_background.tsx
+++ components/timeline/timeline_part_background.tsx
@@ -8,15 +8,18 @@ import treeFive from "public/main_images/timeline/treeFive.png";
 import treeSix from "public/main_images/timeline/treeSix.png";
 import treeSeven from "public/main_images/timeline/treeSeven.png";
 import treeEight from "public/main_images/timeline/treeEight.png";
+import treeNine from "public/main_images/timeline/treeNine.png";
 
 import cloudOne from "public/main_images/timeline/cloudOne.png";
 import cloudTwo from "public/main_images/timeline/cloudTwo.png";
 import cloudThree from "public/main_images/timeline/cloudThree.png";
 import cloudFour from "public/main_images/timeline/cloudFour.png";
 import cloudFive from "public/main_images/timeline/cloudFive.png";
+import cloudSix from "public/main_images/timeline/cloudSix.png";
+import cloudSeven from "public/main_images/timeline/cloudSeven.png";
 
 import Image from "next/image";
-import { Suspense } from "react";
+import { Dispatch, SetStateAction, Suspense, useEffect, useState } from "react";
 import Loading from "@/app/[lang]/loading";
 import { useParams } from "next/navigation";
 
@@ -25,6 +28,30 @@ import { useParams } from "next/navigation";
 
 const TIMELINE_PART_BACKGROUND: () => JSX.Element = (): JSX.Element => {
   const language: string = useParams().lang;
+  const [is_mobile, set_is_mobile]: [
+    boolean[],
+    Dispatch<SetStateAction<boolean[]>>
+  ] = useState<boolean[]>([false, false]);
+
+  useEffect(() => {
+    const handle_resize: () => void = (): void => {
+      set_is_mobile([
+        window.innerWidth <= 350,
+        window.innerWidth <= 400,
+        window.innerWidth <= 450,
+        window.innerWidth <= 550,
+        window.innerWidth <= 975,
+      ]);
+    };
+
+    window.addEventListener("resize", handle_resize);
+
+    handle_resize();
+
+    return () => {
+      window.removeEventListener("resize", handle_resize);
+    };
+  }, []);
 
   return (
     <Suspense fallback={<Loading />}>
@@ -39,7 +66,7 @@ const TIMELINE_PART_BACKGROUND: () => JSX.Element = (): JSX.Element => {
             }
             height={550}
             width={550}
-            className="absolute top-0 left-1/2 -translate-x-1/2 min-w-[550px]"
+            className="absolute top-0 left-1/2 -translate-x-1/2 min-w-[550px] -z-[12]"
           />
         </Suspense>
         <Suspense fallback={<Loading />}>
@@ -52,7 +79,7 @@ const TIMELINE_PART_BACKGROUND: () => JSX.Element = (): JSX.Element => {
             }
             height={550}
             width={550}
-            className="absolute top-[685px] left-1/2 -translate-x-1/2 min-w-[550px]"
+            className="absolute top-[685px] left-1/2 -translate-x-1/2 min-w-[550px] -z-[11]"
           />
         </Suspense>
         <Suspense fallback={<Loading />}>
@@ -65,7 +92,7 @@ const TIMELINE_PART_BACKGROUND: () => JSX.Element = (): JSX.Element => {
             }
             height={550}
             width={550}
-            className="absolute top-[1150px] left-1/2 -translate-x-1/2 min-w-[550px]"
+            className="absolute top-[1150px] left-1/2 -translate-x-1/2 min-w-[550px] -z-[10]"
           />
         </Suspense>
         <Suspense fallback={<Loading />}>
@@ -78,7 +105,7 @@ const TIMELINE_PART_BACKGROUND: () => JSX.Element = (): JSX.Element => {
             }
             height={750}
             width={750}
-            className="absolute top-[1550px] left-1/2 -translate-x-1/2 min-w-[750px] -z-[5]"
+            className="absolute top-[1550px] left-1/2 -translate-x-1/2 min-w-[750px] -z-[9]"
           />
         </Suspense>
         <Suspense fallback={<Loading />}>
@@ -91,7 +118,7 @@ const TIMELINE_PART_BACKGROUND: () => JSX.Element = (): JSX.Element => {
             }
             height={650}
             width={650}
-            className="absolute top-[1950px] left-1/2 -translate-x-1/2 min-w-[650px] -z-[4]"
+            className="absolute top-[1950px] left-1/2 -translate-x-1/2 min-w-[650px] -z-[8]"
           />
         </Suspense>
         <Suspense fallback={<Loading />}>
@@ -104,7 +131,8 @@ const TIMELINE_PART_BACKGROUND: () => JSX.Element = (): JSX.Element => {
             }
             height={750}
             width={750}
-            className="absolute top-[2575px] left-[46%] -translate-x-[54%] min-w-[750px] block sm:hidden -z-[3]"
+            className="absolute top-[2575px] left-[46%] -translate-x-[54%] min-w-[750px] -z-[7]"
+            style={{ display: is_mobile[3] ? "block" : "none" }}
           />
         </Suspense>
         <Suspense fallback={<Loading />}>
@@ -117,10 +145,11 @@ const TIMELINE_PART_BACKGROUND: () => JSX.Element = (): JSX.Element => {
             }
             height={550}
             width={550}
-            className="absolute top-[3150px] left-1/2 -translate-x-1/2 min-w-[550px] block sm:hidden -z-[2]"
+            className="absolute top-[3150px] left-1/2 -translate-x-1/2 min-w-[550px] -z-[6]"
+            style={{ display: is_mobile[2] ? "block" : "none" }}
           />
         </Suspense>
-        {/* <Suspense fallback={<Loading />}>
+        <Suspense fallback={<Loading />}>
           <Image
             src={treeEight}
             alt={
@@ -130,9 +159,24 @@ const TIMELINE_PART_BACKGROUND: () => JSX.Element = (): JSX.Element => {
             }
             height={550}
             width={550}
-            className="absolute top-[3550px] left-1/2 -translate-x-1/2 min-w-[550px] block -z-[1]"
+            className="absolute top-[3550px] left-1/2 -translate-x-1/2 min-w-[550px] -z-[5]"
+            style={{ display: is_mobile[1] ? "block" : "none" }}
           />
-        </Suspense> */}
+        </Suspense>
+        <Suspense fallback={<Loading />}>
+          <Image
+            src={treeNine}
+            alt={
+              language === "de"
+                ? "Achter Baumstamm im Hintergrund"
+                : "Eighth tree trunk in background"
+            }
+            height={650}
+            width={650}
+            className="absolute top-[4000px] left-[46%] -translate-x-[54%] min-w-[650px] -z-[4]"
+            style={{ display: is_mobile[0] ? "block" : "none" }}
+          />
+        </Suspense>
         <Suspense fallback={<Loading />}>
           <Image
             src={cloudOne}
@@ -182,7 +226,7 @@ const TIMELINE_PART_BACKGROUND: () => JSX.Element = (): JSX.Element => {
             }
             height={350}
             width={350}
-            className="absolute top-[1000px] left-[10%] min-w-[350px] opacity-70"
+            className="absolute top-[1000px] left-[10%] min-w-[350px] opacity-70 -z-[8]"
           />
         </Suspense>
         <Suspense fallback={<Loading />}>
@@ -195,7 +239,36 @@ const TIMELINE_PART_BACKGROUND: () => JSX.Element = (): JSX.Element => {
             }
             height={350}
             width={350}
-            className="absolute top-[1400px] left-[17.5%] min-w-[350px] opacity-70"
+            className="absolute top-[1550px] left-[25%] min-w-[350px] opacity-70 -z-[7]"
+            style={{ display: is_mobile[4] ? "block" : "none" }}
+          />
+        </Suspense>
+        <Suspense fallback={<Loading />}>
+          <Image
+            src={cloudSix}
+            alt={
+              language === "de"
+                ? "Sechste Wolke im Hintergrund"
+                : "Sixth cloud in background"
+            }
+            height={350}
+            width={350}
+            className="absolute top-[1850px] right-[16%] min-w-[350px] opacity-70 -z-[9]"
+            style={{ display: is_mobile[3] ? "block" : "none" }}
+          />
+        </Suspense>
+        <Suspense fallback={<Loading />}>
+          <Image
+            src={cloudSeven}
+            alt={
+              language === "de"
+                ? "Siebte Wolke im Hintergrund"
+                : "Seventh cloud in background"
+            }
+            height={350}
+            width={350}
+            className="absolute top-[2400px] right-[25%] min-w-[350px] opacity-70 -z-[3]"
+            style={{ display: is_mobile[2] ? "block" : "none" }}
           />
         </Suspense>
       </div>
