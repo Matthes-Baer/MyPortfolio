fd9b5a8be901b2ef012ab7ef33ffee45a19ad521
Matthes-Baer <Matthes.baer@yahoo.de>
2023-08-18 23:09:25 +0200

    fix: potential fix for production cookie bug

--- middleware.ts
+++ middleware.ts
@@ -22,7 +22,7 @@ export async function middleware(request: NextRequest) {
   //* For revisiting (takes precedence over locale browser settings preference), and for html lang attribute in overall layout
   const language_cookie_bool: boolean = request.cookies.has("language_cookie");
   const language_cookie_value: string =
-    request.cookies.get("language_cookie")?.value || locale;
+    request.cookies.get("language_cookie")?.value || "";
 
   const current_language_path: string = pathname.substring(1, 3);
   const response: NextResponse<unknown> = NextResponse.next();
@@ -38,7 +38,7 @@ export async function middleware(request: NextRequest) {
 
   //* This basically just serves as a condition to execute the NextResponse.redirect() only once and not on every request
   const pathnameIsMissingLocale = supportedLocales.every(
-    (locale) => !pathname.startsWith(`/${locale}`) && pathname !== `/${locale}`
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
   );
 
   //* Redirecting
