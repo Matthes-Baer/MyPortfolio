95a0a769a2e8f85c1403c1382a53497258cc4e7f
Matthes-Baer <Matthes.baer@yahoo.de>
2023-10-27 16:43:55 +0200

    feat: Deleted loading screen from timeline route

--- app/[lang]/main/timeline/loading.tsx
+++
@@ -1,9 +0,0 @@
-const Loading: () => JSX.Element = (): JSX.Element => {
-  return (
-    <div className="w-full h-screen absolute top-0 left-0 flex items-center justify-center">
-      <div className="spinner w-16 h-16 border-4 border-card_yellow border-t-card_blue rounded-full animate-spin"></div>
-    </div>
-  );
-};
-
-export default Loading;
