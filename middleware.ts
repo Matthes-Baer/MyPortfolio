88454b854eef848f74ecf0902eee95d546ae0f80
Matthes-Baer <Matthes.baer@yahoo.de>
2023-10-27 16:14:19 +0200

    middleware test

--- middleware.ts
+++ middleware.ts
@@ -16,52 +16,44 @@ function getLocale(acceptLanguageHeader: string | undefined) {
 }
 
 export async function middleware(request: NextRequest) {
-  const pathname: string = request.nextUrl.pathname;
-  const acceptLanguageHeader = request.headers.get("accept-language");
-  const locale: string = getLocale(acceptLanguageHeader || "");
-
-  //* For revisiting (takes precedence over locale browser settings preference), and for html lang attribute in overall layout
-  const language_cookie_bool: boolean = request.cookies.has("language_cookie");
-  const language_cookie_value: string =
-    request.cookies.get("language_cookie")?.value || "";
-
-  const current_language_path: string = pathname.substring(1, 3);
-  const response: NextResponse<unknown> = NextResponse.next();
-
-  //* Without this check it would automatically be redirected to "de/robots.txt", for example
-  if (
-    pathname === "/robots.txt" ||
-    pathname === "/sitemap.xml" ||
-    pathname === "/favicon.ico"
-  ) {
-    return NextResponse.next();
-  }
-
-  //* This basically just serves as a condition to execute the NextResponse.redirect() only once and not on every request
-  const pathnameIsMissingLocale = supportedLocales.every(
-    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-  );
-
-  //* Redirecting
-  if (pathnameIsMissingLocale) {
-    return NextResponse.redirect(
-      new URL(`/${locale}/${pathname}`, request.url)
-    );
-  }
-
-  //* Updating the language_cookie
-  if (
-    !language_cookie_bool ||
-    (language_cookie_bool && language_cookie_value != current_language_path)
-  ) {
-    const expirationDate: Date = get_expiration_date();
-
-    response.cookies.set("language_cookie", locale, {
-      expires: expirationDate,
-    });
-
-    return response;
-  }
+  // const pathname: string = request.nextUrl.pathname;
+  // const acceptLanguageHeader = request.headers.get("accept-language");
+  // const locale: string = getLocale(acceptLanguageHeader || "");
+  // //* For revisiting (takes precedence over locale browser settings preference), and for html lang attribute in overall layout
+  // const language_cookie_bool: boolean = request.cookies.has("language_cookie");
+  // const language_cookie_value: string =
+  //   request.cookies.get("language_cookie")?.value || "";
+  // const current_language_path: string = pathname.substring(1, 3);
+  // const response: NextResponse<unknown> = NextResponse.next();
+  // //* Without this check it would automatically be redirected to "de/robots.txt", for example
+  // if (
+  //   pathname === "/robots.txt" ||
+  //   pathname === "/sitemap.xml" ||
+  //   pathname === "/favicon.ico"
+  // ) {
+  //   return NextResponse.next();
+  // }
+  // //* This basically just serves as a condition to execute the NextResponse.redirect() only once and not on every request
+  // const pathnameIsMissingLocale = supportedLocales.every(
+  //   (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+  // );
+  // //* Redirecting
+  // if (pathnameIsMissingLocale) {
+  //   return NextResponse.redirect(
+  //     new URL(`/${locale}/${pathname}`, request.url)
+  //   );
+  // }
+  // //* Updating the language_cookie
+  // if (
+  //   !language_cookie_bool ||
+  //   (language_cookie_bool && language_cookie_value != current_language_path)
+  // ) {
+  //   const expirationDate: Date = get_expiration_date();
+  //   response.cookies.set("language_cookie", locale, {
+  //     expires: expirationDate,
+  //   });
+  //   return response;
+  // }
 }
 
 export const config = {
